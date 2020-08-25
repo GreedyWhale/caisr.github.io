f52efa722f6f0ce6cd6101a45f4ac510f9c8d598
caisr <383911973@qq.com>
2020-08-25 16:55:04 +0800

    chore: 完成项目基础环境搭建

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'vue/valid-template-root': 'off',
+    indent: ['error', 2]
+  }
+}
