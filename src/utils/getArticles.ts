207023ce0b164e73999fd24718e11d7555a5165d
caisr <383911973@qq.com>
2020-09-08 14:12:06 +0800

    feature： 完成setup.md迁移

--- src/utils/getArticles.ts
+++ src/utils/getArticles.ts
@@ -2,7 +2,7 @@
  * @Author: MADAO
  * @Date: 2020-09-03 11:34:49
  * @LastEditors: MADAO
- * @LastEditTime: 2020-09-08 11:09:05
+ * @LastEditTime: 2020-09-08 11:45:46
  * @Description: 所有文章组件
  */
 import * as Closure from '/@/articles/javascript/Closure.md'
@@ -23,9 +23,11 @@ import * as MINAGulp from '/@/articles/miniprogram/Gulp.md'
 
 import * as VueCli from '/@/articles/vue/VueCli.md'
 import * as Nuxt from '/@/articles/vue/Nuxt.md'
-import * as ComponentTest from '/@/articles/vue/ComponentTest.md'
+import * as VueComponentTest from '/@/articles/vue/ComponentTest.md'
 
 import * as Calendar from '/@/articles/gadgets/Calendar.md'
+
+import * as PythonSetup from '/@/articles/python/Setup.md'
 
 const formatTime = (timeStr: string): string => {
   const reg = /[年月日]+/g
@@ -48,8 +50,9 @@ const articles = [
   MINAGulp,
   Nuxt,
   VueCli,
-  ComponentTest,
-  Calendar
+  VueComponentTest,
+  Calendar,
+  PythonSetup
 ]
 
 articles.sort((a, b) => {
