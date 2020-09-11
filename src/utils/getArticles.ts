275f342ca4fc881d82f07a1120c308cee0a1dd5d
caisr <383911973@qq.com>
2020-09-11 15:42:44 +0800

    feature: 完成LightAndShadow.md 迁移

--- src/utils/getArticles.ts
+++ src/utils/getArticles.ts
@@ -2,7 +2,7 @@
  * @Author: MADAO
  * @Date: 2020-09-03 11:34:49
  * @LastEditors: MADAO
- * @LastEditTime: 2020-09-11 15:19:59
+ * @LastEditTime: 2020-09-11 15:36:52
  * @Description: 所有文章组件
  */
 import * as JSClosure from '/@/articles/javascript/Closure.md'
@@ -68,6 +68,7 @@ import * as NodeHttp from '/@/articles/node/Http.md'
 import * as NodeExpress from '/@/articles/node/Express.md'
 
 import * as ThreejsTriangle from '/@/articles/threejs/Triangle.md'
+import * as ThreejsLightAndShadow from '/@/articles/threejs/LightAndShadow.md'
 
 const formatTime = (timeStr: string): string => {
   const reg = /[年月日]+/g
@@ -130,7 +131,8 @@ const articles = [
   WebGLTriangle,
   NodeHttp,
   NodeExpress,
-  ThreejsTriangle
+  ThreejsTriangle,
+  ThreejsLightAndShadow
 ]
 
 articles.sort((a, b) => {
