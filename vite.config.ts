0fe6f39575ef1e280e164df322220d7379f08aa8
caisr <383911973@qq.com>
2020-09-17 12:45:22 +0800

    chore: 更新打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: MADAO
  * @Date: 2020-08-31 14:35:32
  * @LastEditors: MADAO
- * @LastEditTime: 2020-09-16 11:03:56
+ * @LastEditTime: 2020-09-17 12:44:06
  * @Description: vite 配置
  */
 const mdPlugin = require('vite-plugin-markdown')
@@ -10,6 +10,8 @@ const path = require('path')
 const { getEntries } = require('./build')
 
 module.exports = {
+  base: '/caisr.github.io/',
+  assetsDir: 'assets',
   alias: {
     '/@/': path.resolve(__dirname, 'src')
   },
